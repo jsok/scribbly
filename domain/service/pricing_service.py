9caef3e8c5093f3cb7febad4e4f5dba1fffaa29d
Jonathan Sokolowski <jonathan.sokolowski@gmail.com>
2013-03-14 15:50:50 +1100

    Add custom exception to Pricing Service if rate not found.

--- domain/service/pricing_service.py
+++ domain/service/pricing_service.py
@@ -9,4 +9,15 @@ class PricingService(Service):
     def get_customer_discount(self, product, customer):
         rate = self.discount_repository.find(product.price_category, customer.discount_tier)
 
+        if not rate:
+            message = "Product Category={0} Discount Tier={1}".format(product.price_category, customer.discount_tier)
+            raise PricingError("Could not find discount rate for: {0}".format(message))
+
         return rate.value
+
+
+class PricingError(Exception):
+    """
+    Generic error if pricing service fails.
+    """
+    pass

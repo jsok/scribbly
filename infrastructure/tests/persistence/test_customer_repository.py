bfe2c274cb7f1d0ba19ee51fc4b65a91a5856f7f
Jonathan Sokolowski <jonathan.sokolowski@gmail.com>
2013-03-27 23:55:08 +1100

    Add simple address persistence test.

--- infrastructure/tests/persistence/test_customer_repository.py
+++ infrastructure/tests/persistence/test_customer_repository.py
@@ -1,6 +1,4 @@
-from unittest import TestCase
-
-from domain.tests.factories.customer import CustomerFactory
+from domain.tests.factories.customer import CustomerFactory, AddressFactory
 
 from infrastructure.tests.persistence import PersistenceTestCase
 from infrastructure.persistence.customer_repository import CustomerRepository
@@ -23,3 +21,23 @@ class CustomerRepositoryTestCase(PersistenceTestCase):
     def test_rollback_performed(self):
         c = self.repository.find("Customer")
         self.assertIsNone(c)
+
+    def test_add_address(self):
+        customer = CustomerFactory.build(name="Customer")
+        self.repository.store(customer)
+
+        biling_address = AddressFactory.build(type="BILLING")
+        customer.add_address(biling_address)
+
+        # Create a shipping address, persist it in a new session
+        shipping_address = AddressFactory.build(type="Shipping")
+        customer.add_address(shipping_address)
+
+        c = self.repository.find("Customer")
+        a = c.get_addresses("BILLING")
+        self.assertIsNotNone(c)
+        self.assertEquals(1, len(a), "Customer should only have 1 billing address")
+        self.assertTrue(biling_address == a[0], "Billing addresses don't match")
+
+        a = c.get_addresses("SHIPPING")
+        self.assertEquals(1, len(a), "Customer should only have 1 shipping addresses")
